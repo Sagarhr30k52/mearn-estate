206627a74b4024268e59b3b3015795328058e65e
Sagar Sharma <sagarsharmahr30k52@gmail.com>
2025-08-07 14:58:12 +0530

    create aa middleware and function to handle possible error

--- api/index.js
+++ api/index.js
@@ -27,3 +27,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🚀`);
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server error';
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
