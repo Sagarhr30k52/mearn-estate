bf8f13eeb2d3f298968ff65da76f7565c5c4352a
Sagar Sharma <sagarsharmahr30k52@gmail.com>
2025-08-16 01:31:13 +0530

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -9,3 +9,19 @@ export const createListing = async (req, res ,next) => {
         next(error);
     }
 };
+
+export const deleteListing = async (req, res, next) => {
+    const listing = Listing.findById(req.params.id);
+    if(!listing){
+        return res.status(404).json({message: "Listing not found"});
+    }
+    if(req.user._id !== listing.userRef){
+        return res.status(403).json({message: "You do not have permission to delete this"});
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json("listing has been deleted");
+    } catch (error) {
+        next(error);
+    }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -105,6 +105,22 @@ function Profile() {
       setShowListingError(true);
     }
   };
+
+  const handleDeleteListing = async (listingId) => {
+      try {
+        const res = await fetch(`/api/listing/delete/${listingId}`, {
+          method: 'DELETE',
+        })
+        const data = await res.json();
+        if (data.success == false) {
+          console.log(data.message);
+          return;
+        }
+        setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+      } catch (error) {
+        console.log(error);
+      }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-bold text-center mb-5">Profile</h1>
@@ -216,7 +232,7 @@ function Profile() {
                   <p>{listing.name}</p>
                 </Link>
                 <div className="flex flex-col items-center">
-                  <button className="text-red-700 uppercase">delete</button>
+                  <button onClick={() => handleDeleteListing(listing._id)} className="text-red-700 uppercase">delete</button>
                   <button className="text-green-700 uppercase">edit</button>
                 </div>
               </div>
