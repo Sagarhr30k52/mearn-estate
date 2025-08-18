a4e5027b24ec1904cd39e72a2961880ab8763e4d
Sagar Sharma <sagarsharmahr30k52@gmail.com>
2025-08-18 11:08:12 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -54,3 +54,40 @@ export const getListing = async (req, res, next) => {
         next(error);
     }
 } 
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = perseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        if(offer === "falsw" || offer === undefined){
+            offer = {$in: [false, true]};
+        }
+        let furnished = req.query.furnished;
+        if(furnished === "false" || furnished === undefined){
+            furnished = {$in: [false, true]};
+        }
+        let parking = req.query.parking;
+        if(parking === "false" || parking === undefined){
+            parking = {$in: [false, true]};
+        }
+        let type = req.query.type;
+        if(type === undefined || type === "all"){
+            type = {$in: ["rent", "sell"]};
+        }
+        const searchTerm = req.query.searchTerm || "";
+        const sort = req.query.sort || "createdAt";
+        const order = req.query.order || "des";
+        const listings = await Listing.find({
+            name: {$regx: searchTerm, $options: 'i'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort({[sort]: order}).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+}
