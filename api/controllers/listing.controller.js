235f2b2fc3473e5aa30b09605c49fe08d25b1777
Sagar Sharma <sagarsharmahr30k52@gmail.com>
2025-08-18 11:09:00 +0530

    search api route complete

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -57,7 +57,7 @@ export const getListing = async (req, res, next) => {
 
 export const getListings = async (req, res, next) => {
     try {
-        const limit = perseInt(req.query.limit) || 9;
+        const limit = parseInt(req.query.limit) || 9;
         const startIndex = parseInt(req.query.startIndex) || 0;
         let offer = req.query.offer;
         if(offer === "falsw" || offer === undefined){
@@ -77,9 +77,9 @@ export const getListings = async (req, res, next) => {
         }
         const searchTerm = req.query.searchTerm || "";
         const sort = req.query.sort || "createdAt";
-        const order = req.query.order || "des";
+        const order = req.query.order || "desc";
         const listings = await Listing.find({
-            name: {$regx: searchTerm, $options: 'i'},
+            name: {$regex: searchTerm, $options: 'i'},
             offer,
             furnished,
             parking,
