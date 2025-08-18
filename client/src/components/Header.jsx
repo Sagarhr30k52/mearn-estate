25880facf051b6509b9f70852242a80a4df3bccf
Sagar Sharma <sagarsharmahr30k52@gmail.com>
2025-08-18 18:57:39 +0530

    complete hearder search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Header() {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
   const { currentUser } = useSelector((state) => state.user.user);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+  
   return (
     <header className="bg-slate-200 shadow-md w-full fixed top-0 right-0 z-10">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-[18px]">
@@ -15,13 +33,17 @@ function Header() {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value = {searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
+          <button>
           <FaSearch className="text-slate-600"></FaSearch>
+          </button>
         </form>
 
         <ul className="flex gap-4 items-center">
